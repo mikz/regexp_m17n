7a8bcf9db824d817e3d1afbcaffe3b7b4351b289
Michal Cichra <michal@o2h.cz>
2014-08-07 18:32:56 +0200

    add 100 encoding tests

--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -5,7 +5,19 @@ require_relative '../lib/regexp_m17n'
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
     Encoding.list.each do |enc|
-      assert(RegexpM17N.non_empty?('.'.encode(enc)))
+      assert(RegexpM17N.non_empty?('.'.force_encoding(enc)))
+    end
+  end
+
+  Encoding.list.each do |enc|
+    name = "test_#{enc.name.downcase.tr('-','_')}"
+    define_method name do
+      str = '.'.force_encoding(enc)
+      begin
+        assert(RegexpM17N.non_empty?(str), "#{enc} matches empty")
+      rescue RegexpError
+        assert nil, "#{enc} raised RegexpError"
+      end
     end
   end
 end
